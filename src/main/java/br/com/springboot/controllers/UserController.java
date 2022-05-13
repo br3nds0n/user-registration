b6562e2f5dd07c07d9e326801935e34d1a8aa4ae
Brendson <brendson.net@gmail.com>
2022-05-13 00:38:49 -0300

    feat: created user model

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,16 +1,29 @@
 package br.com.springboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.model.User;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
   
+  @PostMapping("/")
+  public User create(@RequestBody User user){
+    return user;
+  }
+
   @GetMapping("/")
-  public String helloWorld() {
-    return "hello world";
+  public User read() {
+  }
+
+  @GetMapping("/{id}")
+  public User readId(@PathVariable() Long id){
   }
 
 }
