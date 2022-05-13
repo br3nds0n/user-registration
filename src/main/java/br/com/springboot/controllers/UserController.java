fd21daa035f396ac716300aee476fb18f0b4d545
Brendson <brendson.net@gmail.com>
2022-05-13 01:41:40 -0300

    feat: create conexion database

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,5 +1,8 @@
 package br.com.springboot.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,22 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.model.User;
+import br.com.springboot.repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
   
   @PostMapping("/")
-  public User create(@RequestBody User user){
-    return user;
+  public User create(@RequestBody User user) {
+    return this.userRepository.save(user);
+
   }
 
   @GetMapping("/")
-  public User read() {
-  }
-
-  @GetMapping("/{id}")
-  public User readId(@PathVariable() Long id){
+  public List<User> list() {
+    return this.userRepository.findAll();
   }
 
 }
--- src/main/java/br/com/springboot/model/User.java
+++ src/main/java/br/com/springboot/model/User.java
@@ -1,5 +1,11 @@
 package br.com.springboot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +15,30 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
   private String name;
   private String password;
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
