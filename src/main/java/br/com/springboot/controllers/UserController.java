1129a1e4da831294a7d7edfe35afa98174c02c63
Brendson <brendson.net@gmail.com>
2022-05-14 10:17:18 -0300

    feat: created route update  user

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,18 @@ public class UserController {
   @GetMapping("/{id}")
   public Optional<User> readUser(@PathVariable("id") Long id){
     return this.userRepository.findById(id);
+  }
+
+  @PutMapping("/{id}")
+  public User update(@PathVariable("id") Long id, @RequestBody User user){
+    Optional<User> oldUsers = this.userRepository.findById(id);
+    
+    User newUser = oldUsers.get();
+
+    newUser.setName(user.getName());
+    newUser.setPassword(user.getPassword());
+
+    return this.userRepository.save(newUser);
   }
 
   @DeleteMapping("/{id}")
